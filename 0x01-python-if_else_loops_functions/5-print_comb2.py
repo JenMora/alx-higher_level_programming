ebcdd7c7485a6ca61df50993271c3bc12a18048b
JenMora <jeniffermourah73@gmail.com>
2023-07-11 15:16:16 +0000

    a python function that prints 0-99 in combinations of 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for integers in range(0, 100):
     if integers == 99:
-        print ("{}".format(integers))
+        print("{}".format(integers))
     else:
-        print("{:02d}".format(integers), end=",")
+        print("{:02}".format(integers), end=",")
