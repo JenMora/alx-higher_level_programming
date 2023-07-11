a001820d2148dfa3c9443051b9afa0ae5b7d5243
JenMora <jeniffermourah73@gmail.com>
2023-07-11 20:32:09 +0000

    the first set of combinations of up to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for integers in range(0, 100):
     if integers == 99:
         print("{}".format(integers))
     else:
-        print("{:02}".format(integers), end=',')
+        print("{:02}".format(integers), end=', ')
