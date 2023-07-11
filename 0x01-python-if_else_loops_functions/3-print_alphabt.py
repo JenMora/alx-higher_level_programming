8e6a39b5e9b51853a72565a9db47acf0fb35657c
JenMora <jeniffermourah73@gmail.com>
2023-07-11 14:25:06 +0000

    a python script that prints lowercase alphabet in ASCII exept q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 For alphabet in range(97, 123):
-    if (alphabet == 101) or (alphabet == 113)
+    if (alphabet == 101) or (alphabet == 113):
     continue
 print(chr(alphabet).format(), end="")
