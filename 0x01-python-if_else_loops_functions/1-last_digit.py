6c4c8b8ea6fe9d91fd3b9b7ed520b5623922282b
JenMora <jeniffermourah73@gmail.com>
2023-07-11 18:59:33 +0000

    a program will assign a random signed number to the variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-    last_int = number % 10
-elif number < 0:
-    last_int = number % -10
-    if last_int > 5:
-        print(f"Last digit of {number} is {last_int} and is greayer than 5")
-    elif last_num < 6:
-        print(f" Last digit of {number} is {last_int} and\
-                is less than 6 and not 0")
+last_int = abs(number) % 10
+if number < 0:
+    last_int = -last_int
+        print(f"Last digit of {number:d} is {last_int:d} and is ", end="")
+    if last_num > 5:
+        print("greater than 5")
+    elif last_int == 0:
+        print("0")
     else:
-        print(f"Last digit of {number} is {last_int} and is 0")
+        print("less than 6 and not 0")
