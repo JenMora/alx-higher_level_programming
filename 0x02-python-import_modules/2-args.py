ba2f9c51dbdb412f061cc25c0c66af7b9a258a12
JenMora <jeniffermourah73@gmail.com>
2023-07-13 09:57:24 +0000

    a program that prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """A function that prints the number of arguments parsed\
+            to the command lines"""
+    import sys
+    count = len(sys.argv) -1
+    if count == 0:
+        print("0 argumnets.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print ("{} arguments:".formt(count))
+        for arg in range(count):
+            print ("{}:{}".format(arg + 1, sys.argv[arg + 1]))
