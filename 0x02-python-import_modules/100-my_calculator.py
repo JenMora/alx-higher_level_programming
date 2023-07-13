de40f0334187dd16aec85097b03a235407d1d165
JenMora <jeniffermourah73@gmail.com>
2023-07-13 15:20:10 +0000

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    import calculator_1 as calc
     import sys
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-        at = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(at.keys()):
+        exit(1)
+
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+
+        if sys.argv[2] == '+':
+            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.add(a, b)))
+        elif sys.argv[2] == '-':
+            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.sub(a, b)))
+        elif sys.argv[2] == '*':
+            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.mul(a, b)))
+        elif sys.argv[2] == '/':
+            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.div(a, b)))
+        else:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, at[sys.argv[2]](a, b)))
