3bb80039b0dee491cd629129f2fdcdb2cb65651f
JenMora <jeniffermourah73@gmail.com>
2023-07-14 10:20:34 +0000

    A function that prints integers in a list

--- 0x03-python-data_str/0-print_list_integer.py
+++ 0x03-python-data_str/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """A function that prints all integers in my_list list"""
-    for integer in range(my_list=[]):
+    for integer in my_list:
         print("{:d}".format(integer))
