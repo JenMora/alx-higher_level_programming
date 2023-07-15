a2d80bc5f1b153f9d256d018d0fc4afee04912f4
JenMora <jeniffermourah73@gmail.com>
2023-07-15 16:39:37 +0000

    Matrices can be stubborn at times but with Python, they don't stand a chance

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,14 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+
     """
     Prints a matrix of integers.
-    Args:
+    Arg:
     matrix: A list of lists containing the integers to be printed.
     Returns:
     Nothing.
     """
 
-    for row in matrix:
-        print(" ".join(["{:3d}".format(column) for column in row]))
+    for m in matrix:
+        print(" ".join("{:d}".format(integer) for integer in m))
