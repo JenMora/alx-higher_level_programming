b43da9d8307f207593cd1376c4ca021848109b3a
JenMora <jeniffermourah73@gmail.com>
2023-07-15 13:05:37 +0000

    slicing concept well understood

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
+    if my_list:
+        if idx < 0:
+            return my_list[:]
+    if idx >= len(my_list):
         return my_list[:]
-    if idx > len(my_list):
-        return my_list[:]
-    new_list = my_list[:]
-    new_list[idx] = element
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
     return new_list
