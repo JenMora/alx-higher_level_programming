faaa55912cb4c600728615dec2c710a25167ca33
JenMora <jeniffermourah73@gmail.com>
2023-07-16 18:27:20 +0000

    A function that prints the maximum number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -9,10 +9,9 @@ def max_integer(my_list=[]):
     """
 
     if len(my_list) == 0:
-        return None
-    else:
-        max_integer = my_list[0]
-    for integer in range(len(my_list)):
-        if my_list[integer] > max_integer:
-            max_integer = my_list[integer]
-            return (max_integer)
+        return ("None")
+    integer = my_list[0]
+    for i in my_list:
+        if i > integer:
+            integer = i
+    return (integer)
