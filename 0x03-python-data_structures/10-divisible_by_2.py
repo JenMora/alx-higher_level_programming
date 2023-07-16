0fa31a50ef35465c5c2daaa261a4928b50a2bf6f
JenMora <jeniffermourah73@gmail.com>
2023-07-16 18:58:49 +0000

    a function tha prints numbers divisible by 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for integer in my_list:
+    new_list = my_list[:]
+    for count, integer in enumerate(my_list):
         if integer % 2 == 0:
-            new_list.append(True)
+            new_list[count] = True
         else:
-            new_list.append(False)
+            new_list[count] = False
             return (new_list)
