8c42f51a76d54df0a6343208457143518092c59b
JenMora <jeniffermourah73@gmail.com>
2023-07-16 09:46:32 +0000

    it is a great thing to be resourceful and knowledgeable around git and its commands

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -10,8 +10,9 @@ def max_integer(my_list=[]):
 
     if not my_list: 
         return None
-    max_integer = my_list[0]
-    for integer in my_list:
-        if integer > max_integer:
-            max_integer = integer
-            return max_integer
+    else:
+        max_integer = my_list[0]
+    for integer in range (len(my_list)):
+        if my_list[integer]> max_integer:
+            max_integer = my_list[integer]
+            return (max_integer)
