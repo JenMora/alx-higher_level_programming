4ca0af7fa66f11bf8eb7d7775c7f350971c8cf47
JenMora <jeniffermourah73@gmail.com>
2023-07-17 08:04:20 +0000

    A function that tests the divisibility of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is not None:
-        integer = 0
-        new = []
-        for element in my_list:
-            if my_list[integer] % 2 == 0:
-                new.append(True)
-            else:
-                new.append(False)
-            integer += 1
-        return new
+    new = [True if integer % 2 == 0 else False for integer in my_list]
+    return new
