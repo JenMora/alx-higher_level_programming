11bc5ec68a66e7901c60945f137c71d2c62c72bb
JenMora <jeniffermourah73@gmail.com>
2023-07-18 08:00:45 +0000

    my calculator isnt working

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
-    if len(sys.argv) != 4:
+
+    from sys import argv
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        quit(1)
 
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-
-        if sys.argv[2] == '+':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.add(a, b)))
-        elif sys.argv[2] == '-':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.sub(a, b)))
-        elif sys.argv[2] == '*':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.mul(a, b)))
-        elif sys.argv[2] == '/':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, calc.div(a, b)))
+        a = int(argv[1])
+        b = int(argv[3])
+        ops = ['+', '-', '*', '/']
+        from calculator_1 import add, sub, mul, div
+        functions = [add, sub, mul, div]
+        for integer, number in enumerate(ops):
+            if argv[2] == number:
+                print("{} {} {} = {}".format(a, number, b, functions[integer](a, b)))
+                break
         else:
             print("Unknown operator. Available operators: +, -, * and /")
+            quit(1)
