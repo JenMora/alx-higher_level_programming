230f30f77a72019a31b873ef4f2411b9492d8f8b
JenMora <jeniffermourah73@gmail.com>
2023-07-19 15:08:52 +0000

    Addition in a set of numbers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    '''a function that adds all unique integers\
+        in a list (only once for each integer
+        Arguments:
+        my_list
+        Return:
+        A new list
+        '''
+
+
+    return sum(set(my_list))
