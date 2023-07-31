7670e1fe0af62f0aea451e1e6f8392a1069ac5d3
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-07-31 19:34:51 +0300

    A function that prints an integer in the format {:d}.format

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.11.4
+def safe_print_integer(value):
+    """ A function that prints ann integer with "{:d}".format
+    Agrs:
+    Value(int)
+    Returns
+    false if a type error occurs, otherwise true
+    """
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
