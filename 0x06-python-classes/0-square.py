effd6791c53e7cf552521ed8e396f75b96aa826f
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-08-01 19:57:31 +0300

    An empty class defining a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
-"""
-A python square module illustrating the use of the Square class
+"""this is a documentattion to call the class Square"""
 
-It creates the Square object, prints the square type
-
-and displays its dictionary representation.
-
-"""
 
 class Square:
-    """
-    A class representing a square.
-
-    Attributes:
-        No attributes parsed
-
-    Methods:
-        This in an empty class, no methods used
-    """
+    """Here we through in pass as told in the question"""
     pass
