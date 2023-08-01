e8a32b39154481b1568d1cd9ffa817bebd47534b
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-08-01 20:14:24 +0300

    An empty class defining a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-class Square:
-    """
-    An empty class representing an empty square.
-
+""" An empty class that defines a square
+    
     Attributes:
         No attributes are parsed to this class
 
     Methods:
         No methods used for thnis class
     """
+    class Square:
+        """ This is an empty class, so we pass or use the ellipsis ..."""
     pass
