1d88ef551182bef67a30b123f091922082c5edf2
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-08-08 00:14:19 +0300

    The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -16,17 +16,14 @@ import sys
 
 
 def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column
     for i in range(row):
         if board[i][col] == 1:
             return False
 
-    # Check upper left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check upper right diagonal
     for i, j in zip(range(row, -1, -1), range(col, N)):
         if board[i][j] == 1:
             return False
