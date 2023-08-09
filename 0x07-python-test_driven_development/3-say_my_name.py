cd43f01478cd6cd0608f324b64236ae0b6a87af1
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-08-09 18:05:29 +0300

    A function that prints the first and last names

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains say_my_name function
+The function checks if name is a string
+The function prints first_name and last_name to stdout
+A TypeError is raised when first_name or lastname is not a string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This method prints the first and last name to stdout.
+    Arguments:
+        str: first_name
+        str:last_name
+    Returns: first_name, last_name if both names are strings
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
