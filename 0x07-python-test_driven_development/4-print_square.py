6da9f5e01d4ea9c77b8242c790e06fb5cd63ca31
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-08-09 20:53:07 +0300

    A function that prints a square with teh charcter #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module prints a square with the character #
+"""
+
+
+def print_square(size):
+    """
+    This method prints a square with the character #
+    Args:
+    size: the length of the poison
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif (type(size) is float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for s in range(size):
+        for s_2 in range(size):
+            print("#", end='')
+        print()
