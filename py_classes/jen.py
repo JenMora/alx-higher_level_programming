3c526c4e9fa431402a45104aec5676152ce013d4
JenMora <128831350+JenMora@users.noreply.github.com>
2023-08-10 10:36:29 +0300

    Update jen.py

--- py_classes/jen.py
+++ py_classes/jen.py
@@ -1,13 +1,15 @@
 class Jeniffer:
 
-    def __init__(self, name, age, sign, likes):
+    def __init__(self, name, age, sign, likes, school):
         self.name = name
         self.age = age
         self.sign = sign
         self.likes = likes
+        self.school = school
 
-me = Jeniffer("Jen Doe", 22, "Gem", "codes")
+me = Jeniffer("Jen Doe", 22, "Gem", "codes", "Alx")
 print(me.name)
 print(me.age)
 print(me.sign)
 print(me.likes)
+print(me.school)
