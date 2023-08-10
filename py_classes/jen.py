f301b8e530f030c023c8a2206e0bd28a26c034c5
Jeniffer Moraa Mairura <mjenna303@gmail.com>
2023-08-10 10:32:51 +0300

    I was practising with my calsses and onbjects

---
+++ py_classes/jen.py
@@ -0,0 +1,13 @@
+class Jeniffer:
+
+    def __init__(self, name, age, sign, likes):
+        self.name = name
+        self.age = age
+        self.sign = sign
+        self.likes = likes
+
+me = Jeniffer("Jen Doe", 22, "Gem", "codes")
+print(me.name)
+print(me.age)
+print(me.sign)
+print(me.likes)
