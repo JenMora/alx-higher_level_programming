31c54cc0c175e4e66f6a2c9eb67062d69a120f50
JenMora <jeniffermourah73@gmail.com>
2023-08-10 13:51:28 +0300

    This a trial to check whether I have changed the author of my github

---
+++ py_classes/ajen.py
@@ -0,0 +1,19 @@
+class Jeniffer:
+"""This is a module that describes a person, (me)
+This person is such a genius
+"""
+
+
+    def __init__(self, name, age, sign, likes, school):
+        self.name = name
+        self.age = age
+        self.sign = sign
+        self.likes = likes
+        self.school = school
+
+me = Jeniffer("Jen Doe", 22, "Gem", "codes", "Alx")
+print(me.name)
+print(me.age)
+print(me.sign)
+print(me.likes)
+print(me.school)
