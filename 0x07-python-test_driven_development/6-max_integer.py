f02eb5cef263bea12962c3f4000c09c236087819
JenMora <jeniffermourah73@gmail.com>
2023-08-10 19:23:47 +0300

    A function that prints the max integer in a list to stdout

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""This mdule finds a  max integer in a list
+"""
+This is a module that contains max_integer function with Unittests
+
 """
 
 
 def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
+    """
+    This method finds and returns the max integer in a list of integers
+    The function returns None if the list is empty
+    Arguments:
+        list: The listwith integers
+    Return: max integer in a list of integers, Noe if the kist is empty
     """
     if len(list) == 0:
         return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    ret = list[0]
+    if type(ret)is not int and type(ret) is not float:
+        raise TypeError("result must be an integer")
+    integer = 1
+    while integer < len(list):
+        if list[integer] > ret:
+            ret = list[integer]
+        integer += 1
+    return ret
