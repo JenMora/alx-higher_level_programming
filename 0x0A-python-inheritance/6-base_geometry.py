cf042eaa616a10cc54b3c8044b4b44a0439e6bd0
JenMora <jeniffermourah73@gmail.com>
2023-08-14 19:40:04 +0300

    Amethod that writes a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    This is a class that defines BaseGeometry
+    """
+
+    def area(self):
+        """ This is a method that implements area in BaseGeometry Class
+        args:
+        none
+        Returns:
+        none
+        """
+        raise Exception("area() is not implemented")
