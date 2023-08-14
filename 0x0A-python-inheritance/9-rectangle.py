4d481b5400536c42a5ba797b72b53a9183df7af1
JenMora <jeniffermourah73@gmail.com>
2023-08-14 22:46:39 +0300

    A function that returns the area of a rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A module that defines a class Rectangle that inherits from BaseGeometry
+(7-base_geometry.py). (task based on 8-rectangle.py)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ A method that writes a rectangle that inherits from BaseGeometry
+    (7-base_geometry.py). (task based on 8-rectangle.py)
+
+    Args
+    width: of the rectangle
+    height: of teh rectangle
+
+    Returns: Nothing
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        This method returns the area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """This is teh string representation method"""
+        return "[Rectangle]" + \
+            str(self.__width) + "/" + str(self.__height)
