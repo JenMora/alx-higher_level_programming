4e6e01ca7834f3d3f5219ce8506c9b51d2442f65
JenMora <jeniffermourah73@gmail.com>
2023-08-14 23:15:44 +0300

    A function that writes a square class inheriting from a rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
 """
-This module writes a class Square that inherits
-from Rectangle (9-rectangle.py):
+Module contains Square class
+inherited from Rectangle class
+Prints area
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    this class represents a square
-    Performs instantiation of private instance attribute: size
+    This class inherits from Rectangle class.
+    Performs instantiation of private instance attribute: size.
     """
-
     def __init__(self, size):
         """
-        This method initializes a Square instance.
-        Args:
-            size (int): The size of the square's sides.
+        Initialiazes private instance attribute: size.
         """
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
         """
-        This method calculates the area of the square.
-        Returns:
-            the area of the square.
+        Calculates area.
         """
         return self.__size * self.__size
 
     def __str__(self):
         """
-        Returns a string representation of the Square.
+        Returns informal string representation of an object.
         """
-
         return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
