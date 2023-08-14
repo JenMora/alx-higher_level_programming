510eb15d62c7857a5c1eae47b85495d18788da19
JenMora <jeniffermourah73@gmail.com>
2023-08-14 22:10:19 +0300

    a class BaseGeometry (based on 6-base_geometry.py).

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a BaseGeometry class
+Based on 6-Base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    This is a class that defines BaseGeometry
+    """
+
+    def area(self):
+        """ This is a method that implements area in BaseGeometry Class
+        args:
+        none
+        Returns:
+        none
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This is a method that validates the value argument
+        Args:
+        name: the string argument
+        value: the integer value
+        Returns
+        Nothing
+        """
+        if type(value) != int:
+            raise TypeError ("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError ("{:s} must be greater than 0".format(name))
+
+
+        if __name__ == "__main__":
+            import doctest
+            doctest.testfile("rest/7-base_geometry.txt")
