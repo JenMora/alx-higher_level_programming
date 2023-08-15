1ebe3b6aabb3daee3cee52264d71c5063045a3ec
JenMora <jeniffermourah73@gmail.com>
2023-08-15 22:38:53 +0300

    a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+A module tha defines a class class Student
+"""
+
+
+class Student:
+    """
+    This class defines a student by first_name
+    last_name and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        This method defines a student by first_name
+        last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This public method retrieves a dictionary representation
+        of a student in class Student
+        """
+        if isinstance(attrs, list):
+            stdout = {}
+            for obj in attrs:
+                if isinstance(obj, str) and hasattr(self, obj):
+                    stdout[obj] = getattr(self, obj)
+            return stdout
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        This method replaces all attributes for the student object
+        """
+
+        if "first_name" in json:
+            self.first_name = jason["first_name"]
+        if "last_name" in json:
+            self.last_name = jason["last_name"]
+        if "age_name" in json:
+            self.age = jason["age_name"]
