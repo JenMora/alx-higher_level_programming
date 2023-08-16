84315c571bb264cd8f5cfe104317e725525bd256
JenMora <jeniffermourah73@gmail.com>
2023-08-16 13:31:58 +0300

    This is a correction for the function that inserts a line of text to a file, after each line containing a specific string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -13,23 +13,18 @@ def append_after(filename="", search_string="", new_string=""):
     containing a specific string
     """
 
-    try:
-        with open(filename, 'r') as file:
-            content = file.readlines()
+    content = []
 
-        found = False
-        for i, line in enumerate(content):
-            if search_string in line:
-                content.insert(i + 1, new_string)
-                found = True
-                break
+    with open(filename, 'r', encoding='utf-8') as i_file:
+        # this line of code reads the contens of the file
+        content = i_file.readlines()
+        count = 0
+        while count < len(content):
+            # iterate over each line with a while loop and a counter count
+            if search_string in content[count]:
+                # check of a search_string in each line
+                content[count:count + 1] = [content[count], new_string]
+                count +=1
 
-        if not found:
-            content.append(new_string)
-
-        with open(filename, 'w') as file:
-            file.writelines(content)
-
-    except FileNotFoundError:
-        with open(filename, 'w') as file:
-            file.write(new_string)
+    with open(filename, "w", encoding='utf-8') as i_file:
+            i_file.writelines(content)
