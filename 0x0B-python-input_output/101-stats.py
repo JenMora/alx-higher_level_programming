88e729fc227c4685c2d556fdf93bfb16e33c1006
JenMora <jeniffermourah73@gmail.com>
2023-08-16 14:56:06 +0300

    This is just a trial to the computed metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,52 +1,34 @@
+#!/usr/bin/python3
 import sys
-import signal
+from collections import defaultdict
+"""
+initialize variables to store metrics
+"""
 
-# Define a signal handler for keyboard interruption (CTRL + C)
-def signal_handler(sig, frame):
-    print_metrics()
-    sys.exit(0)
 
-# Register the signal handler
-signal.signal(signal.SIGINT, signal_handler)
-
-# Dictionary to store status code counts
-status_code_counts = {}
-
-# Variable to track total file size
 total_file_size = 0
-
-# Variable to track the line count
+status_code_counts = defaultdict(int)
 line_count = 0
 
-# Function to print metrics
-def print_metrics():
-    print("File size: {}".format(total_file_size))
-    for status_code in sorted(status_code_counts.keys()):
-        print("{}: {}".format(status_code, status_code_counts[status_code]))
-
-# Read input from stdin
 try:
     for line in sys.stdin:
-        try:
-            _, _, _, _, status_code_str, file_size_str = line.split('"')[2].split()
-            status_code = int(status_code_str)
-            file_size = int(file_size_str)
-            
-            total_file_size += file_size
+        _, _, _, _, _, status_code, file_size = line.strip().split(" ")
+        status_code = int(status_code)
+        file_ssize = int(file_ssize)
 
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-            else:
-                status_code_counts[status_code] = 1
+        total_file_size += file_ssize
+        status_code_counts[status_code] += 1
+        line_count += 1
 
-            line_count += 1
+    if line_count % 10 == 0:
+        print("Total file size:", total_file_size)
+    for code in sorted(status_code_counts.keys()):
+        print(f"{code}: {status_code_counts[code]}")
+        print("")
 
-            if line_count % 10 == 0:
-                print_metrics()
+except: KeyboardInterrupt
 
-        except ValueError:
-            pass
-
-except KeyboardInterrupt:
-    print_metrics()
-
+print("\nFinal statistics:")
+print("Total file size:", total_file_size)
+for code in sorted(status_code_counts.keys()):
+    print(f"{code}: {status_code_counts[code]}")
