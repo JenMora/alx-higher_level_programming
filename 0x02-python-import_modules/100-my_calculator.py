cad0a60cf13becef0896f95f2d540ba8abefafc9
JenMora <jeniffermourah73@gmail.com>
2023-08-21 08:49:59 +0300

    untracked file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul,div
 if __name__ == "__main__":
+    arg = len(sys.argv)
+    if arg != 4:
+    a = int(sys.argv((1)))
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-    from sys import argv
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-        a = int(argv[1])
-        b = int(argv[3])
-        ops = ['+', '-', '*', '/']
-        from calculator_1 import add, sub, mul, div
-        functions = [add, sub, mul, div]
-        for integer, number in enumerate(ops):
-            if argv[2] == number:
-                print("{} {} {} = {}".format(a, number, b, functions[integer](a, b)))
-                break
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            quit(1)
+    print(f"{a} {operator} {b} = {result}")
