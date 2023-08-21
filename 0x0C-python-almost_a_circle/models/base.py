89cb7cb9ad37f3bccb70cfacf3cd1254d1149921
JenMora <jeniffermourah73@gmail.com>
2023-08-21 22:49:32 +0300

    i added the models base.py, rectangle.py and square.py models for the project Python almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ This is the base class module"""
 
 
+import json
+
+
 class Base:
     """The base class"""
 
@@ -13,5 +16,51 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects +=1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return a JSON representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        A mrthod that writes the JSON string representation of
+        list_objs to a file
+        """
+
+        if list_objs is None:
+            list_obj = []
+
+        a_file = cls.__name__ + ".json"
+        with open(a_file, "w") as file:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+            json_str = cls.to_json_string(list_dicts)
+            file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(5, 6)  # this is a rectangle dummy instance
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(6)  # this is a square dummy instance
+        else:
+            dummy_instance = None
+        if dummy_instance:
+            dummy_instance.update(**dictionary)
+        return dummy_instance
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This is a module that defines a square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ This is a class quare inheriting from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the side size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Get the size value of the square width"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """sets attributes"""
+        if args:
+            attr = ["size", "id", "x", "y"]
+            for items in range(len(args)):
+                setattr(self, attr[items], args[items])
+            if len(args) >= 2:
+                self.size = args[1]
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == "size":
+                    self.width = value
+                    self.height = value
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """represents a square dictionary"""
+        return {
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y
+                }
