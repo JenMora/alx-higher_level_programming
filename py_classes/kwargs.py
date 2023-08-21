6e38328be172fb2152ad992e0fad38e15b7ca5ee
JenMora <jeniffermourah73@gmail.com>
2023-08-21 05:59:32 +0300

    trying with kwargs

---
+++ py_classes/kwargs.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def greet_me(**kwargs):
+    if kwargs is not None:
+        for key, value in kwargs.items():
+            print ("%s == %s" %(key, value))
+greet_me(name="jenna") 
