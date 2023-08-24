a6fa4ba5e102f9d5ca15b406f3d2ebb4034d1f6a
JenMora <jeniffermourah73@gmail.com>
2023-08-24 22:10:09 +0300

    I added the test cases for class base

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -134,7 +134,8 @@ class TestBase_instantiation(unittest.TestCase):
 
     def test_dict_id(self):
         """This method test for a dictionary id and works correctly"""
-        self.assertEqual({"alpha": 5, "betty": 22}, Base({"alpha": 5, "betty": 22}).id)
+        self.assertEqual({"alpha": 5, "betty": 22}, \n
+                         Base({"alpha": 5, "betty": 22}).id)
 
     def test_bool_id(self):
         """This method sets for a booleon id and works correctly"""
@@ -146,7 +147,7 @@ class TestBase_instantiation(unittest.TestCase):
 
     def test_tuple_id(self):
         """This method checks for a tuple id and works correctly"""
-        self.assertEqual((6, 5,4), Base((6, 5,4)).id)
+        self.assertEqual((6, 5, 4), Base((6, 5, 4)).id)
 
     def test_set_id(self):
         """This method test for a set id and works correctly"""
@@ -154,7 +155,8 @@ class TestBase_instantiation(unittest.TestCase):
 
     def test_frozenset_id(self):
         """ This method tests for a frozen set id and works correctly"""
-        self.assertEqual(frozenset({10, 20, 30}), Base(frozenset({10, 20, 30})).id)
+        self.assertEqual(frozenset({10, 20, 30}), \n
+                         Base(frozenset({10, 20, 30})).id)
 
     def test_range_id(self):
         """This method tests for a range id and works correctly"""
@@ -165,10 +167,10 @@ class TestBase_to_json_string(unittest.TestCase):
     """Unittests for testing to_json_string method of Base class."""
 
     def test_to_json_string_rectangle_type(self):
-         """
-         This method tests if the to_json_string method
-         returns a JSON-formatted string for Rectangle objects.
-         """
+        """
+        This method tests if the to_json_string method
+        returns a JSON-formatted string for Rectangle objects.
+        """
 
         r = Rectangle(12, 17, 20, 18, 16)
         self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
@@ -180,18 +182,18 @@ class TestBase_to_json_string(unittest.TestCase):
         """
 
         r = Rectangle(12, 17, 20, 18, 16)
-        self.assertTrue(len(Base.to_json_string([r.to_dictionary()]))) #  == 53)
+        self.assertTrue(len(Base.to_json_string([r.to_dictionary()])))
 
     def test_to_json_string_rectangle_two_dicts(self):
-         """
-         This is a method that test if the to_json_string method
-         correctly serializes a list of two Rectangle dictionaries.
-         """
+        """
+        This is a method that test if the to_json_string method
+        correctly serializes a list of two Rectangle dictionaries.
+        """
 
         r1 = Rectangle(20, 13, 25, 19, 12)
         r2 = Rectangle(40, 27, 40, 11, 32)
         list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
-        self.assertTrue(len(Base.to_json_string(list_dicts))) #  == 106)
+        self.assertTrue(len(Base.to_json_string(list_dicts)))
 
     def test_to_json_string_square_type(self):
         """
@@ -209,7 +211,7 @@ class TestBase_to_json_string(unittest.TestCase):
         """
 
         s = Square(10, 20, 30, 14)
-        self.assertTrue(len(Base.to_json_string([s.to_dictionary()]))) #  == 39)
+        self.assertTrue(len(Base.to_json_string([s.to_dictionary()])))
 
     def test_to_json_string_square_two_dicts(self):
         """
@@ -220,7 +222,7 @@ class TestBase_to_json_string(unittest.TestCase):
         s1 = Square(10, 2, 3, 4)
         s2 = Square(4, 5, 21, 2)
         list_dicts = [s1.to_dictionary(), s2.to_dictionary()]
-        self.assertTrue(len(Base.to_json_string(list_dicts))) #  == 78)
+        self.assertTrue(len(Base.to_json_string(list_dicts)))
 
     def test_to_json_string_empty_list(self):
         """
@@ -252,7 +254,8 @@ class TestBase_to_json_string(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             Base.to_json_string([], 1)
-    
+
+
 class TestBase_save_to_file(unittest.TestCase):
     """Unittests for testing save_to_file method of Base class."""
 
@@ -290,63 +293,3 @@ class TestBase_save_to_file(unittest.TestCase):
         Square.save_to_file([s])
         with open("Square.json", "r") as f:
             self.assertTrue(len(f.read()) == 39)
-
-    def test_save_to_file_two_squares(self):
-        s1 = Square(10, 7, 2, 8)
-        s2 = Square(8, 1, 2, 3)
-        Square.save_to_file([s1, s2])
-        with open("Square.json", "r") as f:
-            self.assertTrue(len(f.read()) == 77)
-
-    def test_save_to_file_cls_name_for_filename(self):
-        s = Square(10, 7, 2, 8)
-        Base.save_to_file([s])
-        with open("Base.json", "r") as f:
-            self.assertTrue(len(f.read()) == 39)
-
-    def test_save_to_file_overwrite(self):
-        s = Square(9, 2, 39, 2)
-        Square.save_to_file([s])
-        s = Square(10, 7, 2, 8)
-        Square.save_to_file([s])
-        with open("Square.json", "r") as f:
-            self.assertTrue(len(f.read()) == 39)
-
-    def test_save_to_file_None(self):
-        Square.save_to_file(None)
-        with open("Square.json", "r") as f:
-            self.assertEqual("[]", f.read())
-
-    def test_save_to_file_empty_list(self):
-        Square.save_to_file([])
-        with open("Square.json", "r") as f:
-            self.assertEqual("[]", f.read())
-
-    def test_save_to_file_no_args(self):
-        with self.assertRaises(TypeError):
-            Rectangle.save_to_file()
-
-    def test_save_to_file_more_than_one_arg(self):
-        with self.assertRaises(TypeError):
-            Square.save_to_file([], 1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
