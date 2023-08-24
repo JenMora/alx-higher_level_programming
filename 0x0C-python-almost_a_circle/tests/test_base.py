93056cb1def0df250c48cce99ce617f359ff61f9
JenMora <jeniffermourah73@gmail.com>
2023-08-24 23:30:30 +0300

    hot fix on unit tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-Unittest for base
+This is a Unittest module for base ckass
 """
-import unittest
-import json
-import os
+
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import unittest
+import json
+import os
 
 
 class TestBase(unittest.TestCase):
     """
-    Contains unit tests for the Base class.
+    This class contains unit tests for the Base
     """
 
     def test_base_id(self):
         """
-        Test id.
+        This method is for testing any kind of id
         """
         obj1 = Base()
         self.assertEqual(obj1.id, 1)
@@ -32,20 +33,9 @@ class TestBase(unittest.TestCase):
 
     def test_to_json_string(self):
         """
-        Tests to_json_string method.
+        This method tests to_json_string
         """
-        obj1 = Square(10, 2, 8)
-        self.assertEqual(type(obj1), Square)
-        dict_sq = obj1.to_dictionary()
-        json_dict_sq = Base.to_json_string([dict_sq])
-        json_dict_sq_1 = Base.to_json_string([])
-        json_dict_sq_2 = Base.to_json_string(None)
-        self.assertEqual(type(json_dict_sq), str)
-        from_json = json.loads(json_dict_sq)
-        self.assertEqual(json_dict_sq_1, "[]")
-        self.assertEqual(json_dict_sq_2, "[]")
-        self.assertDictEqual(from_json[0], dict_sq)
-        obj = Rectangle(10, 7, 2, 8)
+        obj = Rectangle(7, 8, 10, 9)
         self.assertEqual(type(obj), Rectangle)
         dictionary = obj.to_dictionary()
         json_dictionary = Base.to_json_string([dictionary])
@@ -56,43 +46,36 @@ class TestBase(unittest.TestCase):
         self.assertEqual(json_dictionary_1, "[]")
         self.assertEqual(json_dictionary_2, "[]")
         self.assertDictEqual(from_json[0], dictionary)
+        obj1 = Square(4, 8, 2)
+        self.assertEqual(type(obj1), Square)
+        dict_sq = obj1.to_dictionary()
+        json_dict_sq = Base.to_json_string([dict_sq])
+        json_dict_sq_1 = Base.to_json_string([])
+        json_dict_sq_2 = Base.to_json_string(None)
+        self.assertEqual(type(json_dict_sq), str)
+        from_json = json.loads(json_dict_sq)
+        self.assertEqual(json_dict_sq_1, "[]")
+        self.assertEqual(json_dict_sq_2, "[]")
+        self.assertDictEqual(from_json[0], dict_sq)
 
-    def test_from_json_string(self):
+    def test_base_id(self):
         """
-        Tests from_json_string method.
+        This method is for testing any kind of id
         """
-        list_input = [
-                {'id': 89, 'width': 10, 'height': 4},
-                {'id': 7, 'width': 1, 'height': 7}
-                ]
-        json_list_input = Rectangle.to_json_string(list_input)
-        list_output = Rectangle.from_json_string(json_list_input)
-        self.assertEqual(type(list_output), list)
-        self.assertEqual(list_output, list_input)
-        json_list_input = Rectangle.to_json_string([])
-        list_output = Rectangle.from_json_string(json_list_input)
-        self.assertEqual(list_output, [])
-
-    def test_create(self):
-        """
-        Tests create method.
-        """
-        r1 = Rectangle(3, 5, 1)
-        self.assertEqual(type(r1), Rectangle)
-        r1_dictionary = r1.to_dictionary()
-        r2 = Rectangle.create(**r1_dictionary)
-        self.assertEqual((r1 is r2), False)
-        self.assertEqual((r1 == r2), False)
-        s1 = Square(2, 1, 2)
-        self.assertEqual(type(s1), Square)
-        s1_dictionary = s1.to_dictionary()
-        s2 = Square.create(**s1_dictionary)
-        self.assertEqual((s1 is s2), False)
-        self.assertEqual((s1 == s2), False)
+        obj1 = Base()
+        self.assertEqual(obj1.id, 1)
+        obj2 = Base()
+        self.assertEqual(obj2.id, 2)
+        obj3 = Base()
+        self.assertEqual(obj3.id, 3)
+        obj4 = Base(12)
+        self.assertEqual(obj4.id, 12)
+        obj5 = Base()
+        self.assertEqual(obj5.id, 4)
 
     def test_load_from_file(self):
         """
-        Tests load_from_file method.
+        This method tests load_from_file
         """
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
@@ -110,6 +93,39 @@ class TestBase(unittest.TestCase):
         for i in range(2):
             self.assertEqual(str(list_sq_input[i]), str(list_sq_output[i]))
             self.assertNotEqual(list_sq_input[i], list_sq_output[i])
+
+    def test_from_json_string(self):
+        """
+        This method tests from_json_string
+        """
+        list_input = [
+                {'id': 89, 'width': 10, 'height': 4},
+                {'id': 7, 'width': 1, 'height': 7}
+                ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(type(list_output), list)
+        self.assertEqual(list_output, list_input)
+        json_list_input = Rectangle.to_json_string([])
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_output, [])
+
+    def test_create(self):
+        """
+        This method tests creation of the
+        """
+        r1 = Rectangle(5, 7, 3)
+        self.assertEqual(type(r1), Rectangle)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual((r1 is r2), False)
+        self.assertEqual((r1 == r2), False)
+        s1 = Square(4, 2, 4)
+        self.assertEqual(type(s1), Square)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertEqual((s1 is s2), False)
+        self.assertEqual((s1 == s2), False)
 
     def tearDown(self):
         """
