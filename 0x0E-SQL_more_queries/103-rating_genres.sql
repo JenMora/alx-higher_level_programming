3b7fc07d854a373b2dc2d886609bbdc5eef5bb15
JenMora <jeniffermourah73@gmail.com>
2023-09-20 12:01:44 -0700

    a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- a script that lists all genres in the database hbtn_0d_tvshows_rate by their ratin
+SELECT tg.name, IFNULL(SUM(tsr.rate), 0) AS rating
+FROM tv_genres AS tg
+LEFT JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
+LEFT JOIN tv_shows AS ts ON tsg.show_id = ts.id
+LEFT JOIN tv_show_ratings AS tsr ON ts.id = tsr.show_id
+GROUP BY tg.name
+ORDER BY rating DESC;
