2de4bd3d20b8bffff299561d6d510acb337040df
JenMora <jeniffermourah73@gmail.com>
2023-09-20 11:20:07 -0700

    a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,11 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tsg
+ON ts.id = tsg.show_id
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM tv_show_genres AS tsg2
+  WHERE tsg2.show_id = ts.id
+)
+ORDER BY ts.title, tsg.genre_id;
