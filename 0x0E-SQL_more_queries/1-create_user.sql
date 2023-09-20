3e64f1619200ff150fe4fdd4b2af6def1470ba2a
JenMora <jeniffermourah73@gmail.com>
2023-09-20 09:03:56 -0700

    It checks if the user user_0d_1 already exists in the MySQL server by querying the mysql.user table. If the user exists, it sets the variable @user_exists to 1; otherwise, it sets it to 0.
    If the user does not exist (@user_exists is 0), it creates the user user_0d_1 with the specified password user_0d_1_pwd.
    
    It grants all privileges on all databases and tables (*.*) to user_0d_1 and includes the WITH GRANT OPTION to allow the user to grant privileges to other users. This effectively gives the user full control over the MySQL server.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,10 +2,6 @@
 -- user_0d_1 should have all privileges on your MySQL server
 -- user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
--- Check if the user exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') INTO @user_exists;
-
 -- If the user doesn't exist, create it with all privileges
-IF @user_exists = 0 THEN
   CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
   GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
