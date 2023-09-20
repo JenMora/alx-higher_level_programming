e0a1a9c87821a5cd3a0f29e040bb2edb2f6f3c77
JenMora <jeniffermourah73@gmail.com>
2023-09-20 09:02:11 -0700

    It checks if the user user_0d_1 already exists in the MySQL server by querying the mysql.user table. If the user exists, it sets the variable @user_exists to 1; otherwise, it sets it to 0.
    If the user does not exist (@user_exists is 0), it creates the user user_0d_1 with the specified password user_0d_1_pwd.
    
    It grants all privileges on all databases and tables (*.*) to user_0d_1 and includes the WITH GRANT OPTION to allow the user to grant privileges to other users. This effectively gives the user full control over the MySQL server.
    
    After creating the user and granting privileges, it flushes privileges to make the changes take effect.
    
    It provides a message indicating whether the user was created or if the user already existed.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -8,14 +8,4 @@ SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'loca
 -- If the user doesn't exist, create it with all privileges
 IF @user_exists = 0 THEN
   CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-  GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-  FLUSH PRIVILEGES;
--- this statement is used to reload the server's privilege tables, which
--- store information about user accounts and their associated privileges.
--- When you execute commands that modify user privileges or create new users
--- (such as CREATE USER, GRANT, REVOKE, ALTER USER, etc.)
-  SELECT 'User user_0d_1 created and granted all privileges.';
-ELSE
-  SELECT 'User user_0d_1 already exists.';
-END IF;
-
+  GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
