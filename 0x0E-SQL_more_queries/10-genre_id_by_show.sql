1133adedee303435d8bb1906f900a8e23eeb29f6
JenMora <jeniffermourah73@gmail.com>
2023-09-20 11:15:15 -0700

    a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at
+-- least one genre linked.
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg
+ON tsg.show_id = ts.id
+ORDER BY ts.title, tsg.genre_id ASC;
