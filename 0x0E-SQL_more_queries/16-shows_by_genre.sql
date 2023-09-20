ef1dcf79d2a076d3720ddf4cc4c2787fbfd5e8bd
JenMora <jeniffermourah73@gmail.com>
2023-09-20 11:47:17 -0700

    a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,10 @@
 -- a script that lists all shows, and all genres linked
 -- to that show, from the database hbtn_0d_tvshows
-SELECT tg.name
-FROM tv_genres AS tg
-INNER JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
-INNER JOIN tv_shows AS ts ON tsg.show_id = ts.id
-WHERE ts.title = "Dexter"
-ORDER BY tg.name;
+SELECT ts.title, tg.name
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tsg
+  ON ts.id = tsg.show_id
+LEFT JOIN tv_genres AS tg
+  ON tsg.genre_id = tg.id
+ORDER BY ts.title, tg.name;
+
