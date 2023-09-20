f76e018b183633a975a6708ad0559bd63d749c88
JenMora <jeniffermourah73@gmail.com>
2023-09-20 09:13:59 -0700

    a script that creates the database hbtn_0d_2 and the user user_0d_2.
    user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
    The user_0d_2 password should be set to user_0d_2_pwd
    If the database hbtn_0d_2 already exists, your script should not fail
    If the user user_0d_2 already exists, your script should not fail

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,8 +4,6 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-IF @user_exists = 0 THEN
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
-
