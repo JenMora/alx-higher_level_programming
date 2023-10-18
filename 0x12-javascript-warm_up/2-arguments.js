60fcbc8dea8ea203fdb37d4c6091ae8a9a1821bb
JenMora <jeniffermourah73@gmail.com>
2023-10-18 11:34:04 -0700

    Task 2 correction, I corrected the argument indices

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const argc = process.argv.slice(2);
-if (argc === 0) {
+const argc = process.argv.length;
+if (argc === 2) {
   console.log('No argument');
-} else if (argc === 1) {
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
