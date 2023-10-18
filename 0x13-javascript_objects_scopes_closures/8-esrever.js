c50441271b8b4f926287cee3ed0d514d3f79423a
JenMora <jeniffermourah73@gmail.com>
2023-10-18 09:42:07 +0300

    a function that returns the reversed version of a list:

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,8 +3,7 @@
 exports.esrever = function (list) {
   const temp = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    const value = list[i];
-    temp.push(value);
+    temp.push(list[i]);
   }
   return temp;
 };
