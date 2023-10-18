0fa892fa25880a84bce2f00fd74f83b84f94fe02
JenMora <jeniffermourah73@gmail.com>
2023-10-18 10:40:36 +0300

    This script imports the dictionary of occurrences by user ID, iterates through it, and creates a new dictionary where the keys are the number of occurrences and the values are lists of user IDs. Finally, it prints the new dictionary containing user IDs by occurrence.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const dictUser = require('./101-data.js').dictUser;
-const newDictUser = {};
-for (const key in dictUser) {
-  if (!newDictUser[dictUser[key]]
-    newDictUser[dictUser[key]] = [];
+const dataModule = require('./101-data.js');
+const dict = dataModule.dict;
+const newDict = {}; // create new dictionary
+for (const user in dict) { // loop through original dict
+  const key = dict[user];
+  if (newDict[key]) { // check if key exists
+    newDict[key].push(user); // add user id
+  } else {
+    newDict[key] = [user]; // create new
   }
-  newDictUser[dictUser[key]].push(key);
 }
-console.log(newDictUser);
+console.log(newDict);
