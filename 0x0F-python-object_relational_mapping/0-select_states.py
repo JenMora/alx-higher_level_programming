4267e011812bf849b88aea606e2d272589e1cfe8
JenMora <jeniffermourah73@gmail.com>
2023-10-21 03:33:52 -0700

    a script that lists all states from the database hbtn_0e_0_usa....

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
-import MySQLdb
-import sys
-
-
-def list_states(username, password, database_name):
-    """
-    List all states from a MySQL database.
+"""
+ List all states from a MySQL database.
 
     Args:
         username (str): MySQL username for authentication.
@@ -20,35 +14,31 @@ def list_states(username, password, database_name):
         This function connects to a MySQL server and retrieves a list of
         states from a specific database.
         It sorts the results by the 'id' column and displays them.
-    """
-    try:
-        # Connect to the MySQL server
-        conn = MySQLdb.connect(host='localhost', port=3306,
-                               user=mysql_username, passwd=mysql_password,
-                               db=database_name, charset="utf8")
-        cursor = conn.cursor()
+"""
 
-        # Retrieve the states by executing the SQL query
-        cursor.execute("SELECT id, name  FROM states ORDER BY states.id ASC")
-        tables = cursor.fetchall()
-
-        # Display the results
-        for state in tables:
-            print(state)
-
-        # Close the cursor and connection
-        cursor.close()
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
+import MySQLdb
+import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database_name>")
-    else:
-        mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
-        list_states(mysql_username, mysql_password, database_name)
+        sys.exit(1)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    """
+    Connects to a MySQL server running on localhost at port 3306
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                           passwd=mysql_password, db=database_name,
+                           charset="utf8")
+    """
+    Cursor object to interact with database
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
