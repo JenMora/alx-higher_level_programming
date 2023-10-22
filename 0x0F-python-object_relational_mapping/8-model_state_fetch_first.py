68ca4fa4960557496f774a1b7d9f757bc6cbed8f
JenMora <jeniffermourah73@gmail.com>
2023-10-22 02:16:22 -0700

    a script that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Prints the first State object from the 'hbtn_0e_6_usa' database
+arguments:
+    mysql username
+    mysql password
+    database name
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get the command-line arguments
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create a MySQL database connection
+    db_url = f'mysql+mysqldb://{db_user}:{db_password}@localhost:3306/{db_name}'
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    # Create a database session
+    session = Session(engine)
+
+    # Query the first State object
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    # Close the database session
+    session.close()
