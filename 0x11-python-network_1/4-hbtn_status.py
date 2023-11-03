a1ff9e7761f071d8365d28ee32c521b9ca0ecd4a
JenMora <jeniffermourah73@gmail.com>
2023-11-03 12:20:08 -0700

    a Python script that fetches https alx status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ A Python script that fetches https://intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+          .format(type(r.text), r.text))
