c2abf1fc711f37c4943e3bc5543145af5bb6c797
JenMora <jeniffermourah73@gmail.com>
2023-11-29 03:45:02 -0800

    a script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,29 +2,19 @@
 const request = require('request');
 const fs = require('fs');
 
-// Check if both arguments are provided
-if (process.argv.length !== 4) {
-  console.error('Usage: node fetchAndStore.js <url> <file-path>');
-  process.exit(1); // Exit with an error code
-}
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Make a request to the specified URL
 request(url, (err, response, body) => {
   if (err) {
     console.error(err);
-  } else if (response.statusCode === 200) {
-    // Write the body response to the specified file
-    fs.writeFile(filePath, body, 'utf-8', (writeErr) => {
+  } else {
+    fs.writeFile(filePath, body, (writeErr) => {
       if (writeErr) {
         console.error(writeErr);
       } else {
         console.log(`Content successfully written to ${filePath}`);
       }
     });
-  } else {
-    console.error(`Error: Unable to fetch content from ${url}. Status code: ${response.statusCode}`);
   }
 });
